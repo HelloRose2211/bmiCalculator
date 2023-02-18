a5f06fb0d630c748d310cc5071f0c4f4ca83b792
Rose Khaldeeva <khalrose@mail.ru>
2023-02-18 17:53:29 +0600

    Initial Commit

---
+++ bmiCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmiCalculator
+//
+//  Created by Rose on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
